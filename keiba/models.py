aefb6ecb2de504f7ffc9e0cc2d2314180049fcc0
Shizuo Ichitake <harmony.tk74@gmail.com>
2022-06-27 21:38:21 +0900

    keibaアプリケーションのモデルを変更

--- keiba/models.py
+++ keiba/models.py
@@ -1,8 +1,25 @@
+from tabnanny import verbose
 from django.db import models
+from enum import Enum
+
+class Sex(Enum):
+    MALE = 'M'
+    FEMALE = 'F'
 
 # Create your models here.
 class Horse(models.Model):
     class Meta:
-        db_table = 'horse'
+        db_table = 'horses'
     
     name = models.CharField(verbose_name='名前', max_length=255, unique=False, null=False)
+    age = models.IntegerField(verbose_name='年齢', unique=False, null=False)
+    sex = models.CharField(verbose_name='性別', max_length=1, unique=False, null=False)
+
+    def sex_text(self):
+        if self.sex == Sex.MALE.value:
+            return '牡'
+        else:
+            return '牝'
+
+    def __str__(self):
+        return f'name = {self.name}, age = {self.age}, sex = {self.sex_text()}'
--- keiba/views.py
+++ keiba/views.py
@@ -1,9 +1,17 @@
 from django.views import View
 from django.shortcuts import render
+import keiba
 
 # Create your views here.
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'keiba/index.html')
+        horse_list = \
+            [ {'name': h.name, 'age': h.age, 'sex': h.sex_text()} for h in keiba.models.Horse.objects.all() ]
+
+        context = {
+            'horse_list': horse_list
+        }
+
+        return render(request, 'keiba/index.html', context)
 
 index = IndexView.as_view()
